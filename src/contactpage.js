75f9e892c681b464e20ec92944f2c70236ad499a
Mindshaked <eduardgb92@gmail.com>
2022-04-24 17:35:14 +0200

    setting up contact page

--- src/contactpage.js
+++ src/contactpage.js
@@ -2,7 +2,9 @@ const contactPage = function loadContactPage(){
     const content = document.getElementById("content");
 
     const contactContainer = document.createElement("div");
-    contactContainer.setAttribute("id", "contact-container")
+    contactContainer.setAttribute("id", "contact-container");
+
+    
 
   
     content.appendChild(contactContainer);
